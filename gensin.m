40de2ede3d38216e1e48bd01d9ae0c3b278c8068
hanishkvc <hanishkvc@gmail.com>
2006-12-28 17:16:00 +0530

    Get FreqSpectrum instead of FreqResponse
    * Added a function which generates the frequence spectrum from the given time
    domain data using fft. If sampling rate is not given it is assumed as 44.1KHz.
    Also a additional freqsubmult argument is given, this helps restrict the freq
    to which the fft result is restricted.
    
    Thus if no argument other than time domain data is given, it is assumed to be
    a sampled at the popular audio frequency of 44.1Khz and inturn freq data upto
    22.05Khz will be returned, because anything above 22.05Khz will not be sampled
    in any meaningfull way at all with a sampling frequency of 44.1Khz (niquist
    theorem).
    
    Rather ideally the sampling rate should be atleast 10 times the maximum freq
    to be properly monitored.
    
    * Corrected the variable name from fnamegen to evalstr
    
    * Fixed a bug wrt the logic which was generating the samples for a given bit
    resolution. (2**bits-1) to ((2**bits-1)/2)
    
    * Added logic to cross verify (reverify) that the generated data which is saved
    into /tmp/data_.... files is proper by reloading and ploting the same wrt time
    and freq domains.

--- gensin.m
+++ gensin.m
@@ -1,11 +1,12 @@
-# Simple logic to generate a sine wave at 
-#   a given frequency, for a given duration, and a given db
-# v27Dec2006
+# Simple logic to generate sine waves at 
+#   given frequencies, for a given duration, and a given set of dBs
+#   It also plots each wave and its freq spectrum
+# v28Dec2006
 # HanishKVC, 20Dec2006
 #
 
-
 debug=1
+reverify=1
 if(debug==0)
   freqs=[50,100,200,400,600,800,1000,2000,4000,8000,10000,12000,14000,16000,18000,20000]
   samprate=44100
@@ -13,47 +14,73 @@ if(debug==0)
 else
   freqs=[50,400,1000,8000]
   samprate=44100*1.0
-  ampsdB=[-67, -7, 0, 6]
+  ampsdB=[-67, -7, 0, 3, 6]
 endif
 
 duration=5
 ampMax=10**0.6
-
 bits=16
 
-gset terminal jpeg size 800,800
+function [hist,freq] = freq_spectrum(data, samplingrate,freqsubmult)
+  if(nargin < 3)
+    freqsubmult = 2;
+  endif
+  if(nargin < 2)
+    samplingrate = 44100;
+  endif
+
+  datalen=columns(data);
+  hist=fft(data);
+  freq=(1:datalen)*(samplingrate/datalen);
+
+  hist=hist(1:datalen/freqsubmult);
+  freq=freq(1:datalen/freqsubmult);
+  #plot(freq,abs(hist));
+endfunction
+
+entries=columns(freqs)+1;
+evalstr=strcat("gset terminal jpeg size 1200,",int2str(200*entries))
+eval(evalstr,"gset terminal jpeg size 1200,800")
 
 for a=1:columns(ampsdB)
 
   multiplot(0,0)
   printf(strcat("\n*** Working for ",int2str(ampsdB(a)),"dB ***\n"))
-  fnamegen=strcat("gset output \"/tmp/plot_",int2str(ampsdB(a)),".jpg\"")
-  eval(fnamegen,"gset output test.jpg")
-  
-  ampratio=10**(ampsdB(a)/10)/ampMax
-  dataAll=0
-  if(debug==1)
-    multiplot(2,5)
+  evalstr=strcat("gset output \"/tmp/plot_",int2str(ampsdB(a)),"dB.jpg\"")
+  eval(evalstr,"gset output /tmp/plot.jpg")
+  if(reverify==1)
+    multiplot(4,entries)
   else
-    multiplot(2,8)
+    multiplot(2,entries)
   endif
+  
+  ampratio=(10**(ampsdB(a)/10)/ampMax) # -0.01
+  dataAll=0
   for i=1:columns(freqs)
     printf(strcat("Generating freq",int2str(freqs(i)),"...\n"))
-    data=ampratio*sin(2*pi*((1:samprate*duration)/samprate)*freqs(i))*(2**bits-1);
-    if(debug == 1)
-      mplot(data(1:150))
-      [ra,rf]=freqz(data,1,[],samprate);
+
+    data=ampratio*sin(2*pi*((1:samprate*duration)/samprate)*freqs(i))*((2**bits-1)/2);
+    data=round(data);
+    mplot(data(1:150))
+    [ra,rf]=freq_spectrum(data,samprate);
+    mplot(rf,abs(ra))
+    
+    fname=strcat("/tmp/data_",int2str(ampsdB(a)),"dB_",int2str(freqs(i)));
+    saveaudio(fname,data,"raw",bits)
+    if(reverify==1)
+      dataT=loadaudio(fname,"raw",bits);
+      dataT=dataT';
+      mplot(dataT(1:150))
+      [ra,rf]=freq_spectrum(dataT,samprate);
       mplot(rf,abs(ra))
     endif
-    saveaudio(strcat("/tmp/data_",int2str(ampsdB(a)),"_",int2str(freqs(i))),data,"raw",bits)
+
     dataAll=dataAll+data;
   endfor
+
   printf "Generating the freq response...\n"
   mplot(dataAll(1:150))
-  [ra,rf]=freqz(dataAll,1,[],samprate);
+  [ra,rf]=freq_spectrum(dataAll,samprate);
   mplot(rf,abs(ra))
-  #title(strcat("Plots for ",int2str(ampsdB(a)),"dB"))
-  #printf "Press any key to continue...\n"
-  #pause
 endfor
 
