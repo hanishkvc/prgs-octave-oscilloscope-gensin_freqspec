16e1d38a66f25ada443b1ff328da0a3831aedaaf
hanishkvc <hanishkvc@gmail.com>
2007-01-11 23:59:59 +0530

    Restructuring, additions and experiments
    * Renamed some variables to make them bit more verbose
    
    * added freq_spectrum_log which basically converts the histogram data wrt
      the frequencies from the fft logic using log10
    
    * added a freq_gen function which uses the freq generation equation used
      before also.
    
    * added a freq_gen_square function, which generates square wave by looking
      at sign of the value in a sine wave and multiplying it with the max value
      from the sine wave.
    
    * moved the existing/full samples generation logic into a function called
      samples_gen
    
    * added a test_diffDBs to check how the data is different in time and freq
      domain for a given frequency and different amplitude levels
    
    * added db_find function which given a value, maximum value allowed and what
      is the dB level associated with the max value specified (have to rethink the
      reasoning behind what I had done in 2007 now in 2017 now, wrt the maxdb once
      again later) will tell the dB corresponding to given value
    
    * misc experimentations like
      * test_repDataOverwrite - octave understanding experiment
      * gen_1_0 - generate a series of alternating 1s and 0s (have to recheck once
        wrt what reshape does).
    
    * added harmonic_gen which given a fundamental frequency and the range of its
      harmonics to be generated, will generate two sets of samples. 1st set is for
      the combined base+harmonics samples and the 2nd set is purely the base freq
      samples
    
    * added freq_spectrum_log_findabove which given a time domain data with the
      sampling rate at which it was captured. Will generate the log10 based
      histogram or power info wrt the frequencies in that time domain data, and
      will inturn eliminate/clip all power/hist data which is below a give min
      and above a given max value.
      Then it will print all frequencies which are above the specified min value
      and their corresponding clipped power/hist value.
      It will also plot the same.
    
    * added a util_lecroydata_plot function, which given a plot data file from
      lecroy oscilloscope will load and inturn find the freq spectrum of the same
      and plot it.

--- gensin.m
+++ gensin.m
@@ -1,25 +1,26 @@
 # Simple logic to generate sine waves at 
 #   given frequencies, for a given duration, and a given set of dBs
 #   It also plots each wave and its freq spectrum
-# v28Dec2006
+# v12Jan2007
 # HanishKVC, 20Dec2006
 #
 
-debug=1
-reverify=1
+global debug=1
+global reverify=1
 if(debug==0)
-  freqs=[50,100,200,400,600,800,1000,2000,4000,8000,10000,12000,14000,16000,18000,20000]
-  samprate=44100
-  ampsdB=[-67, -47, -27, -7, 0, 3, 6]
+  frequencies=[50,100,200,400,600,800,1000,2000,4000,8000,10000,12000,14000,16000,18000,20000]
+  samplingrate=44100
+  amplitudesDB=[-67, -47, -27, -7, 0, 3, 6]
 else
-  freqs=[50,400,1000,8000]
-  samprate=44100*1.0
-  ampsdB=[-67, -7, 0, 3, 6]
+  #frequencies=[50,400,1000,8000]
+  frequencies=[50, 100, 200, 400, 800, 1000, 2000, 4000, 8000, 10000, 12000, 14000, 16000, 18000, 20000]
+  samplingrate=44100*1.0
+  amplitudesDB=[-67, -7, 0, 3, 6]
 endif
 
-duration=5
-ampMax=10**0.6
-bits=16
+duration=20
+amplitudeMax=10**0.6
+bitspersample=16
 
 function [hist,freq] = freq_spectrum(data, samplingrate,freqsubmult)
   if(nargin < 3)
@@ -38,49 +39,163 @@ function [hist,freq] = freq_spectrum(data, samplingrate,freqsubmult)
   #plot(freq,abs(hist));
 endfunction
 
-entries=columns(freqs)+1;
-evalstr=strcat("gset terminal jpeg size 1200,",int2str(200*entries))
-eval(evalstr,"gset terminal jpeg size 1200,800")
+function [hist,freq]=freq_spectrum_log(data,samplingrate,freqsubmult)
+  [hist,freq]=freq_spectrum(data,samplingrate,freqsubmult);
+  hist=log10(hist);
+endfunction
 
-for a=1:columns(ampsdB)
+function data = freq_gen(freq, samprate, duration, ampratio, bits)
+  data=ampratio*sin(2*pi*(freq/samprate)*(1:samprate*duration))*(2**(bits-1)-1);
+  data=round(data);
+endfunction
 
-  multiplot(0,0)
-  printf(strcat("\n*** Working for ",int2str(ampsdB(a)),"dB ***\n"))
-  evalstr=strcat("gset output \"/tmp/plot_",int2str(ampsdB(a)),"dB.jpg\"")
-  eval(evalstr,"gset output /tmp/plot.jpg")
-  if(reverify==1)
-    multiplot(4,entries)
-  else
-    multiplot(2,entries)
-  endif
-  
-  ampratio=(10**(ampsdB(a)/10)/ampMax) # -0.01
-  dataAll=0
-  for i=1:columns(freqs)
-    printf(strcat("Generating freq",int2str(freqs(i)),"...\n"))
+function data = freq_gen_square(freq, samprate, duration, ampratio, bits)
+  data=freq_gen(freq,samprate,duration,ampratio,bits);
+  #data=sign(data)*min(data);
+  data=sign(data)*max(data);
+endfunction
 
-    data=ampratio*sin(2*pi*((1:samprate*duration)/samprate)*freqs(i))*((2**bits-1)/2);
-    data=round(data);
-    mplot(data(1:150))
-    [ra,rf]=freq_spectrum(data,samprate);
-    mplot(rf,abs(ra))
-    
-    fname=strcat("/tmp/data_",int2str(ampsdB(a)),"dB_",int2str(freqs(i)));
-    saveaudio(fname,data,"raw",bits)
+function samples_gen(freqs,samprate,duration,ampsdB,ampMax,bits)
+  global debug
+  global reverify
+
+  entries=columns(freqs)+1;
+  evalstr=strcat("gset terminal jpeg size 1600,",int2str(200*entries))
+  eval(evalstr,"gset terminal jpeg size 1200,800")
+
+  for a=1:columns(ampsdB)
+
+    fflush(stdout);
+    multiplot(0,0)
+    printf(strcat("\n*** Working for ",int2str(ampsdB(a)),"dB ***\n"))
+    evalstr=strcat("gset output \"/tmp/plot_",int2str(ampsdB(a)),"dB.jpg\"")
+    eval(evalstr,"gset output /tmp/plot.jpg")
     if(reverify==1)
-      dataT=loadaudio(fname,"raw",bits);
-      dataT=dataT';
-      mplot(dataT(1:150))
-      [ra,rf]=freq_spectrum(dataT,samprate);
-      mplot(rf,abs(ra))
+      multiplot(4,entries)
+    else
+      multiplot(2,entries)
     endif
+  
+    ampratio=(10**(ampsdB(a)/10)/ampMax) # -0.01
+    dataAll=0
+    for i=1:columns(freqs)
+      printf(strcat("Generating freq",int2str(freqs(i)),"...\n"))
 
-    dataAll=dataAll+data;
+      data=ampratio*sin(2*pi*((1:samprate*duration)/samprate)*freqs(i))*((2**bits/2)-1);
+      data=round(data);
+      mplot(data(1:150))
+      [ra,rf]=freq_spectrum(data,samprate);
+      mplot(rf,abs(ra))
+    
+      fname=strcat("/tmp/data_",int2str(ampsdB(a)),"dB_",int2str(freqs(i)));
+      saveaudio(fname,data,"raw",bits)
+      if(reverify==1)
+        dataT=loadaudio(fname,"raw",bits);
+        dataT=dataT';
+        mplot(dataT(1:150))
+        [ra,rf]=freq_spectrum(dataT,samprate);
+        mplot(rf,abs(ra))
+      endif
+
+      dataAll=dataAll+data;
+    endfor
+
+    printf "Generating the freq response...\n"
+    mplot(dataAll(1:150))
+    [ra,rf]=freq_spectrum(dataAll,samprate);
+    mplot(rf,abs(ra))
   endfor
+  multiplot(0,0)
+endfunction
 
-  printf "Generating the freq response...\n"
-  mplot(dataAll(1:150))
-  [ra,rf]=freq_spectrum(dataAll,samprate);
-  mplot(rf,abs(ra))
-endfor
+function test_diffDBs()
+  multiplot(2,4);
+  
+  d1=freq_gen(1000,44100,5,0.5,16);
+  mplot(d1(1:150))
+  [rh,rf]=freq_spectrum(d1);
+  mplot(rf,rh);
+  
+  d2=freq_gen(2000,44100,5,1,16);
+  mplot(d2(1:150))
+  [rh,rf]=freq_spectrum(d2);
+  mplot(rf,rh);
+  
+  d3=freq_gen(1000,44100,5,1,16);
+  mplot(d3(1:150))
+  [rh,rf]=freq_spectrum(d3);
+  mplot(rf,rh);
+  
+  d=d1+d2+d3;
+  mplot(d(1:150))
+  [rh,rf]=freq_spectrum(d);
+  mplot(rf,rh);
+  
+  pause
+  multiplot(0,0)
+endfunction
+
+function dB=dB_find(value,maxvalue,maxdb)
+  dB=log10(value*(10**(maxdb/10))/maxvalue)*10
+endfunction
+
+function data=test_repDataOverwrite(data,interval)
+  for i=1:interval:columns(data)
+    data(i)=data(i+1);
+  endfor
+endfunction
+
+function [dh,df]=harmonic_gen(fundfreq, hstart, hint, hstop, harmamp)
+  df=freq_gen(fundfreq,44100,2,1,16);
+  dh=df;
+  for i=hstart:hint:hstop
+    dt=freq_gen(fundfreq*i,44100,2,harmamp,16);
+    dh=dh+dt;
+  endfor
+endfunction
+
+function data=gen_1_0(count)
+  a1=ones(1,count/2);
+  a2=zeros(1,count/2);
+  aT=[a1; a2];
+  data=reshape(aT,1,count);
+endfunction
+
+function freq_spectrum_log_findabove(data,samplingrate,freqsubmult, min,max)
+  [powers,freqs]=freq_spectrum_log(data,samplingrate,freqsubmult);
+  powers=abs(powers);
+  powers_c=clip(powers,[min,max]);
+  powers_c=powers_c-min;
+  ti=find(powers_c);
+  printf ("freqs => log10(powers)_above_%g\n",min);
+  for i = 1:length(ti)
+    printf ("%g => %g(%g)\n",freqs(ti(i)),min+powers_c(ti(i)),powers_c(ti(i)));
+  endfor
+  plot(freqs,powers_c);
+endfunction
+
+function util_lecroydata_plot(datafile,samplingrate,freqsubmult,min,max)
+  evalstr=strcat("dataT=load ",datafile,";")
+  eval(evalstr,"printf \"Error loading data\n\"");
+  data=dataT(1:length(dataT),2)';
+  freq_spectrum_log_findabove(data,samplingrate,freqsubmult,min,max);
+endfunction
+
+function dummy_init
+  printf("Welcome to gensin.m v12Jan2007\n");
+endfunction
+
+dummy_init
+#test_diffDBs()
+#samples_gen(frequencies,samplingrate,duration,amplitudesDB,amplitudeMax,bitspersample)
+
+# misc
+#[r1,r2]=freqz(sA,1,[],44100);
+#plot(r2,r1), hold
+#min,max,range,clip
+#values,who,load, save,
+#abs,real,floor,round,ceil
+#find,nonzeros
+
+
 
